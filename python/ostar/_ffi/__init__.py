d67f114bd39814e12bc9108e581f055026d7fae4
m0dulo <yuhsin.lyx@gmail.com>
2023-08-14 14:03:49 +0800

    Create __init__.py

---
+++ python/ostar/_ffi/__init__.py
@@ -0,0 +1,4 @@
+from . import _pyversion
+from .base import register_error
+from .registry import register_object, register_func, register_extension
+from .registry import _init_api, get_global_func, get_object_type_index
